d354455426146fd38962093fb3d6b9ac92386b6e
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-07-10 19:20:09 +0300

    adding cards

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "../css/main.css";
 import pizza1 from "../images/pizza1.png";
 import pizzavid from "../videos/pizzaVid.mp4";
+import { ReactComponent as CartSvg } from "../svg/cart.svg";
+
 class Main extends Component {
   constructor(props) {
     super(props);
@@ -9,9 +11,37 @@ class Main extends Component {
       pizza: [
         {
           id: 1,
+          name: "Uncle john",
+          price: "15$",
+          src: pizza1,
+          desc: "black olives mushrooms and red bell peppers",
+          className: "main-pizza",
+        },
+        {
+          id: 2,
           name: "margarita",
           price: "15$",
           src: pizza1,
+          desc:
+            " all-time classic fresh tomato sauce and sizzling mozzarella cheese ",
+          className: "main-pizza",
+        },
+        {
+          id: 3,
+          name: "margarita",
+          price: "15$",
+          src: pizza1,
+          desc:
+            " all-time classic fresh tomato sauce and sizzling mozzarella cheese ",
+          className: "main-pizza ",
+        },
+        {
+          id: 4,
+          name: "margarita",
+          price: "15$",
+          src: pizza1,
+          desc:
+            " all-time classic fresh tomato sauce and sizzling mozzarella cheese ",
           className: "main-pizza",
         },
       ],
@@ -25,28 +55,46 @@ class Main extends Component {
       <div className="container-fluid p-0 main">
         <div className="container-fluid p-0">
           <video
-            autoPlay
-            muted
-            className="pizza-video"
+            muted={true}
+            playsinline
             loop
+            autoPlay
+            playsInline
             src={pizzavid}
+            className="pizza-video"
           ></video>
         </div>
 
-        <div className="container pizza-header">
+        <div className="container pizza-header text-center">
           <p>Order one of our amazing pizza's Today!</p>
         </div>
-        <div className="pizza mx-auto">
-          {pizza.map((pizza) => {
-            return (
-              <img
-                key={pizza.id}
-                src={pizza.src}
-                className={pizza.className}
-                alt="pizza"
-              />
-            );
-          })}
+        <div className="pizzas container-fluid  ">
+          <div className="row">
+            {pizza.map((pizza) => {
+              return (
+                <div className=" col-xl-3 col-lg-4 col-md-6  pizza-card ">
+                  <div
+                    className="card  mx-auto p-0 m-3"
+                    style={{ width: "19rem" }}
+                  >
+                    <img
+                      key={pizza.id}
+                      src={pizza.src}
+                      className={pizza.className + "card-img-top"}
+                      alt="pizza"
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{pizza.name}</h5>
+                      <p className="card-text">{pizza.desc}</p>
+                      <a href="#a" className="btn btn-primary addToCart">
+                        Add to cart <CartSvg className="cart" />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     );
